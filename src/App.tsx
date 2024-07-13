cefa99f937dc894193c77da5f181eaa0ebbeac7a
Ayush Dayama <ayushdayama097@gmail.com>
2024-07-13 22:44:38 +0530

    Ready for public release

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,73 @@
 import { Counter } from "./components/Counter";
-import rocketSvg from "./assets/img/EditedBgRmUs.png";
-import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.scss";
+import toast, { Toaster } from "react-hot-toast";
+import { useState, useCallback, useRef } from "react";
+import Confetti from "react-confetti";
 
 export default function App() {
-  const notify = () =>
-    toast.info("I love you moreeee ❤️", {
-      closeButton: false,
-      theme: "colored",
+  const [isToastVisible, setIsToastVisible] = useState(false);
+  const [confettiProps, setConfettiProps] = useState({
+    run: false,
+    recycle: true,
+  });
+  const timeoutRef = useRef<number | null>(null);
+
+  const stopConfetti = useCallback(() => {
+    setConfettiProps((prev) => ({ ...prev, recycle: false }));
+    timeoutRef.current = window.setTimeout(() => {
+      setConfettiProps({ run: false, recycle: true });
+    }, 5000); // Allow 5 seconds for the last pieces to fall
+  }, []);
+
+  const notify = () => {
+    setIsToastVisible(true);
+    setConfettiProps({ run: true, recycle: true });
+    toast("💞 2nd Dec", {
+      duration: 3000,
+      style: {
+        fontSize: "1.6rem",
+        padding: "10px",
+        minWidth: "200px",
+        marginTop: "15vh",
+        backgroundColor: "#ac9386",
+        color: "white",
+      },
     });
 
+    // Stop generating new confetti after 3 seconds
+    if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    timeoutRef.current = window.setTimeout(stopConfetti, 3000);
+  };
+
   return (
-    <main>
+    <main className={isToastVisible ? "blurred" : ""}>
+      <Confetti
+        run={confettiProps.run}
+        recycle={confettiProps.recycle}
+        numberOfPieces={200}
+        gravity={0.1}
+      />
       <div className="container">
         <h1>Kavita 💍 Ayush</h1>
 
         <Counter />
 
-        <p>Until we two become one</p>
+        <p>💞</p>
 
-        <button onClick={notify}>I have something to tell you</button>
-
-        <ToastContainer
-          theme="dark"
-          position="top-center"
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          pauseOnHover
-        />
+        <button className="button" onClick={notify}>
+          💍 Aur Shaadi ki date?
+          <div className="hoverEffect">
+            <div></div>
+          </div>
+        </button>
       </div>
-
-      <img src={rocketSvg} alt="" width={500} />
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          className: "custom-toast",
+        }}
+      />
     </main>
   );
 }
